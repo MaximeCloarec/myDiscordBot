e6bf97840c7e365c5833c2097807279976bdfac0
MaximeCloarec <156484577+MaximeCloarec@users.noreply.github.com>
2025-06-17 21:35:22 +0200

    Commande pokemon ajout choix nom

--- commands/utility/apiCall.js
+++ commands/utility/apiCall.js
@@ -3,10 +3,30 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("api-call")
-        .setDescription("Call an api"),
+        .setDescription("Call an api")
+        .addStringOption((option) =>
+            option
+                .setName("pokemonname")
+                .setDescription("The name of the Pokémon to fetch data for")
+                .setRequired(true)
+        ),
+
     async execute(interaction) {
-        var data = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-        data = await data.json();
-        await interaction.reply(`${data} Caca`);
+        const pokemonName = interaction.options.getString("pokemonname");
+        console.log(`Fetching data for Pokémon: ${pokemonName}`);
+
+        try {
+            var data = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+            );
+            console.log(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            data = await data.json();
+            await interaction.reply(`Nom du pokémon : ${data.forms[0].name}`);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(
+                "Une erreur s'est produite lors de l'appel à l'API. Ou le Pokémon n'existe pas."
+            );
+        }
     },
 };
